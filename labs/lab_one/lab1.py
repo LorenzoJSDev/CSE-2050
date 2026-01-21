99c1c693688b7bd650a689cc5f368ce15ca985eb
Lorenzo Serrano <lorenzo.serrano@ibm.com>
2026-01-21 15:31:29 -0500

    finshed lab 1

---
+++ labs/lab_one/lab1.py
@@ -0,0 +1,11 @@
+def generic_hi(name="world"):
+    return f"Hello {name}!"
+
+
+
+assert generic_hi() == "Hello world!"
+
+assert generic_hi("Ada") == "Hello Ada!"
+
+assert generic_hi("CSE2050") == "Hello CSE2050!"
+
