1dd5546016f5cf4d50a02b9e8999682cad726dd3
Lorenzo Serrano <lorenzo.serrano@ibm.com>
2026-01-28 14:40:38 -0500

    feat(lab_two/Foo.py) Finshed Foo.py for lab2

---
+++ labs/lab_two/Foo.py
@@ -0,0 +1,36 @@
+class Foo:
+    """This is the Foo Class"""
+
+    def __init__(self, name: str, profession: str) -> None:
+        """
+        Docstring for __init__
+        
+        :param self: 
+        :param name: Foo name
+        :type name: str
+        :param profession: Foo profession
+        :type profession: str
+        """
+        self.name = name
+        self.profession = profession
+
+    def __repr__(self) -> str:
+        """
+        Docstring for __repr__
+        
+        :param self: 
+        :return: "Foo(<name>, <profession>)"
+        :rtype: str
+        """
+        return f"Foo({self.name}, {self.profession})"
+    
+    def speak(self) -> str:
+        """
+        Docstring for speak
+        
+        :param self: 
+        :return: "<name> says hello!"
+        :rtype: str
+        """
+        return  f"{self.name} says hello!"
+
