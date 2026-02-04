672cef94044c2a97e50e4619d116707d6933ac24
Lorenzo Serrano <lorenzo.serrano@ibm.com>
2026-02-04 14:41:04 -0500

    daily

--- Notes/moduleThree.py
+++ Notes/moduleThree.py
@@ -13,6 +13,9 @@ Last Updated: 01-28-2026
 
 """
 
+# !!!! I NEED TO GO BACK AND TAKE BETTER NOTES ON THIS SECTION !!!! #
+
+
 #=== Imports ===#
 import unittest #Standard Library import
 
@@ -39,5 +42,77 @@ def test_driven_development():
     refactor, remove all the fat
     """
 
-#Private instances do not get tested via unittest
+# Private instances do not get tested via unittest
 
+# Q: How do decided if your code is efficient?
+# A: Time it takes for code to run, resources it takes to complete the task
+
+
+# Thinking about effeicenty helps us develop good habits, intution and design descisons
+
+# Measure effecinty: Measure time it takes for program to run, count the number of operations the program executes
+
+
+def running_time():
+    """
+    Defintion: The number of atomic operrations is uesed to describe the running time of an algorithm
+
+    Atomic Operations:
+        * Atherimetic and boolean operations
+        * Object Creation
+        * Varible assignment
+        * Branching
+        * Calling a function
+        * Returning from a function
+    """
+    y = 0 # Two atomic operations
+    x = [j for i in range(1,10) for j in range(1,10)] # 81 atomic operations
+
+    return x,y
+
+"""
+Chcking the running time of a fucntion
+
+def f001(L):
+    new list = []   # 2 operations, creation and assignment
+
+
+"""
+
+def asymptotic_order_of_growth():
+    """
+    Defintion: 
+    """
+
+    pass
+
+
+def asymptotic_notation():
+    """
+    Definition: Used to describe the running time of an algorithm (how much time an algo takes given input n)
+
+
+    """
+    pass
+
+
+def find_target(L,t):
+    """
+
+    Class room examples for Aysmototic order of growth
+
+    """
+    for i in L: #... n loops times
+        if i == t: #... 2 operations, if and compare
+            return True # ...  1 operation
+    return False  # ...  1 operation
+
+n = 10
+L = [i for i in range (1,n+1)]
+print(L)
+find_target(L, 1)
+
+
+# !!!! I NEED TO GO BACK AND TAKE BETTER NOTES ON THIS SECTION !!!! #
+
+# In this lecture it was important to be able to know what time in Asymtotic notation a particular function ran in O(N+M) etc
