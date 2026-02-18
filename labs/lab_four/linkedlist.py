a7d4d7fd866416c010aaecb0af3afec378b0c464
jjabraham012 <jjabraham012@gmail.com>
2026-02-18 15:57:32 -0500

    something

--- labs/lab_four/linkedlist.py
+++ labs/lab_four/linkedlist.py
@@ -19,10 +19,25 @@ from typing import Optional
 
 class Node:
     def __init__(self, item, link: Optional['Node'] = None) -> None:
+        """
+        Docstring for __init__
+        
+        :param self: Description
+        :param item: Description
+        :param link: Description
+        :type link: Optional['Node']
+        """
         self.item = item
         self.link = link
 
     def __repr__(self) -> str:
+        """
+        Docstring for __repr__
+        
+        :param self: Description
+        :return: Description
+        :rtype: str
+        """
         return f"Node(item:{self.item}, data: {self.link})"
 
 class LinkedList:
--- labs/lab_four/test_linkedlist.py
+++ labs/lab_four/test_linkedlist.py
@@ -2,7 +2,7 @@
 <test_linkedlist.py>
 
 -------------------------------------------------------------
-This is a script is a peice of lab four
+This is a script is a piece of lab four
 -------------------------------------------------------------
 
 Author: Lorenzo .S
@@ -14,18 +14,34 @@ Last Updated: Created: 02-18-2026
 
 #=== Imports ===#
 
+import unittest
 from unittest import TestCase
 from linkedlist import Node, LinkedList
 
 #=== Classes ===#
 
 class TestNode(TestCase):
-    def setup(self):
+    def setUp(self):
         self.node1 = Node("data")
         self.node2 = Node("2data2", self.node1)
 
-    def test_init(self):
+    def test_init_no_link(self):
         self.assertEqual(self.node1.item, 'data')
-        self.assertEqual(self.node1.item, None)
+        self.assertIsNone(self.node1.link)
+
+    def test_init_yes_link(self):
+        self.assertEqual(self.node2.item, '2data2')
+        self.assertEqual(self.node2.link, self.node1)
+
+    def test_repr_no_link(self):
+        expected = "Node(item:data, data: None)"
+        self.assertEqual(repr(self.node1), expected)
+
+    def test_repr_yes_link(self):
+        expected = f"Node(item:2data2, data: {repr(self.node1)})"
+        self.assertEqual(repr(self.node2), expected)
 
     # self.assertEqual(self.a1.name, 'Arthur')
+
+if __name__ == "__main__":
+    unittest.main()
