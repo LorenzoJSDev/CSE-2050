fb468116752936528c633911794005122c69616e
Lorenzo Serrano <lorenzo.serrano@ibm.com>
2026-02-18 11:12:15 -0500

    notes mod4

---
+++ Notes/moduleFour/moduleFour.py
@@ -0,0 +1,27 @@
+"""
+<moduleFour.py>
+
+-------------------------------------------------------------
+Notes for CSE 2050 module four which covers:
+
+- Abstract Data Types
+    * Stack
+    * Queue
+    * Deque (deck)
+
+- Data Structure
+    * List
+    * Linked list
+    * Doubly Linked List
+
+-------------------------------------------------------------
+
+Author: Lorenzo .S
+Contributors: [" "]
+Created: 02-18-2026
+Last Updated: Created: 02-18-2026
+
+"""
+
+
+#---- 02-18-2026 Lecuture Notes ----#
