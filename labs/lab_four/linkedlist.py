212c7e6b7c2a36b26c761a6931322c24b36ad1cf
Lorenzo Serrano <lorenzo.serrano@ibm.com>
2026-02-18 15:16:27 -0500

    finshed Node class

--- labs/lab_four/linkedlist.py
+++ labs/lab_four/linkedlist.py
@@ -11,14 +11,19 @@ Created: 02-18-2026
 Last Updated: Created: 02-18-2026
 
 """
+#=== Imports ===#
+from typing import Optional
+
+#=== Classes ===#
+
 
 class Node:
-    def __init__(self, item, link: Node = None) -> None:
-        self.this = item
+    def __init__(self, item, link: Optional['Node'] = None) -> None:
+        self.item = item
         self.link = link
 
     def __repr__(self) -> str:
-        return f"{self.item},{self.link}"
+        return f"Node(item:{self.item}, data: {self.link})"
 
 class LinkedList:
     pass
--- labs/lab_four/test_linkedlist.py
+++ labs/lab_four/test_linkedlist.py
@@ -11,3 +11,21 @@ Created: 02-18-2026
 Last Updated: Created: 02-18-2026
 
 """
+
+#=== Imports ===#
+
+from unittest import TestCase
+from linkedlist import Node, LinkedList
+
+#=== Classes ===#
+
+class TestNode(TestCase):
+    def setup(self):
+        self.node1 = Node("data")
+        self.node2 = Node("2data2", self.node1)
+
+    def test_init(self):
+        self.assertEqual(self.node1.item, 'data')
+        self.assertEqual(self.node1.item, None)
+
+    # self.assertEqual(self.a1.name, 'Arthur')
