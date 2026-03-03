786b354e587741203aed2f13dedb53853e663b35
Lorenzo Serrano <lorenzo.serrano@ibm.com>
2026-03-03 14:13:32 -0500

    milestone1

--- Notes/test.py
+++ Notes/test.py
@@ -1,13 +1,16 @@
-
-class A:
-  def __init__(self):
-    self.x = 1
+import random, sys
+random.seed(658)
+sys.setrecursionlimit(1000000)
  
-class B(A):
-  def __init__(self):
-    super().__init__()
-    self.y = 2
+def foo(counter=0):
+    """Generates random numbers until we get a 50"""
+    # Track how many calls we've made
+    counter += 1
  
-obj = B()
+    random_number = random.randint(1, 100)
+    if random_number == 50:
+        return
+    
+    foo(counter)
  
-print(obj.x)
+foo()
