a822549ae518f27334c25962dca21e1e2428aca4
Lorenzo Serrano <lorenzo.serrano@ibm.com>
2026-03-04 15:54:57 -0500

    working on data_handler.py

---
+++ milestones/milestoneOne/data_handler.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+data_handler.py
+-------------
+
+Description: Handles uploaded data, and data query's
+
+Author: Lorenzo .S
+Contributors:
+Date Created: 03-04-2026
+Status: Development (alpha)
+
+"""
+
+
+# ===== Imports =====
+
+# Standard library
+import csv
+
+# Third-party
+
+
+# Local application (your project modules)
+
+
+# ===== Classes =====
+
+class DataHandler():
+    """
+    Docstring for Course class
+        - Description: The data_handler class for milestone one, handles loading data, and data query's for milestoneOne project
+        - Author: Lorenzo .S
+    """
+
+    def __init__(self, university_data, course_catalog) -> None:
+        """
+        Docstring for __init__
+            - Description: TBD
+            - Author: Lorenzo .S
+
+        """
+
+        pass
+
