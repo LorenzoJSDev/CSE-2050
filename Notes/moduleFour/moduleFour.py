b96fb22cf43f6d6c2069be327e4d503ec29604ea
Lorenzo Serrano <lorenzo.serrano@ibm.com>
2026-03-09 20:17:37 -0400

    help

--- Notes/moduleFour/moduleFour.py
+++ Notes/moduleFour/moduleFour.py
@@ -17,185 +17,622 @@ Notes for CSE 2050 module four which covers:
 -------------------------------------------------------------
 
 Author: Lorenzo .S
-Contributors: [" "]
+Contributors:
 Created: 02-18-2026
 Last Updated: Created: 02-18-2026
 
 """
 
-def ADT():
-    """
-    Docstring
-
-    """
-
-
-#---- 02-18-2026 Lecuture Notes ----#
-
+#==== Imports ====#
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
+#-----------------------------------#
 
-class Stack:
-    """ 
-    TBD
+class ADT:
+    """
+    Docstring for moduleFour.ADT()
+
+    Definition:
+        - Class of objects whose logical behavior is defined by a set of values and operations.\
+        - Logical models
+        - Tells what methods the data structure will implement
+        - Putting rules on data Structures
+
+    Examples of ADTs:
+        * Stack
+        * Queue
+        * Deque (deck)
     """
     pass
 
-# You take a data structure like a list, and modify it so it only has the fucntionality you want it to
-
-class Queue:
+class DataStructure:
     """
-    Definition:
+    Docstring for moduleFour.DataStructure()
 
-    Main operations:
-        * enqueue(element)
-        * dequeue(element)
+    Definition:
+        - An implementation of ADT(logical models)
+        - Concrete representations fo data
+
+    Examples of Data Structures:
+        * List
+        * Linked List
+        * Doubly Linked List
+    """
+    pass
+
+
+#---- 02-18-2026 Lecture Notes ----#
+
+#==== Abstract Datatypes ====#
+class Stack(ABC):
+    """ 
+    Docstring for moduleFour.Stack
+
+    Definition:
+        - A list with the restriction that insertion and deletion can only be preformed from one end.
+        - The end is also called the top.
+        - LIFO: Last in First Out
+
+    Stack Diagram:
+    Stack = [1,2,3,4,5,6,7]
+                         ^
+                         Head
+    Main Operations:
+        * Push(item)    # TIME COMPLEXITY: O(1)
+        * Pop()         # TIME COMPLEXITY: O(1)
+        * Top or Peek() # TIME COMPLEXITY: O(1)
+        * IsEmpty()     # TIME COMPLEXITY: O(1)
+        * Len()         # TIME COMPLEXITY: O(1)
+            - usually just the default __len__ function in python
+
+    """
+
+    def __init__(self):
+        """
+        Docstring for moduleFour.Stack.__init__()
+
+        When used as a wrapper (for a list as an example) it would init self.L = []
+        since it is imposing rules on the list object [] is would need to wrap around the list []
+        Get it?
+        """
+        return
+
+    @abstractmethod
+    def push(self, item):
+        """
+        Docstring for moduleFour.Stack.push()
+
+        Time complexity: O(1)
+        Description:
+            - Adds a new item to the stack.
+            - Places a new item at the top of the stack.
+
+        Example:
+        Stack = [1,2,3,4,5,6,7]
+        Stack.push(8)
+        Stack = [1,2,3,4,5,6,7,8]
+                               ^
+                               New Head
+        """
+        return
+
+    @abstractmethod
+    def pop(self):
+        """
+        Docstring for moduleFour.Stack.pop()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Removes and RETURNS the item at the top of the stack.
+            - RETURNS the item at the top of the stack.
+
+        Example:
+        Stack = [1,2,3,4,5,6,7]
+                             ^
+                             Head
+        Stack.pop()     #pop() removes the last item from a list by default
+
+        Stack = [1,2,3,4,5,6]
+                           ^
+                           New Head
+        RETURNS: 7
+        """
+        return
+
+    @abstractmethod
+    def peek(self):
+        """
+        Docstring for moduleFour.Stack.peek()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Returns the item at the top of the stack.
+            - Returns the head of the stack.
+            - peek() can also be known as top()
+
+        Example:
+            Stack = [1,2,3,4,5,6,7]
+            Stack.peek()    # Does not affect the stack itself.
+            returns: 7
+        """
+        return
+
+    @abstractmethod
+    def __len__(self):
+        """
+        Docstring for moduleFour.Stack.__len__()
+
+        Time complexity: O(1)
+        Description:
+            - Returns the number of items in the stack.
+            - Can also be known as size()
+        """
+        return
+
+    @abstractmethod
+    def is_empty(self):
+        """
+        Docstring for moduleFour.Stack.isEmpty()
+
+        Time complexity: O(1)
+        Description:
+            - Returns True if the stack is empty, False otherwise.
+            - How this is coded depends on the data structure the stack class is wrapping around.
+        """
+        return
+
+# Example of Stack Wrapper for List
+class ListStack(Stack):
+    """
+    Docstring for moduleFour.ListStack(Stack)
+
+    Description:
+        - This is an implementation of the Stack class as a wrapper for a list object.
+        - The stack class imposes rules on the list such as things can only be added and taken from the head/top of the stack/back of the list.
+    """
+
+    def __init__(self):
+        """
+        Docstring for moduleFour.ListStack(Stack).__init__()
+
+        Description:
+            - This implementation creates a new ListStack object.
+            - Since we are wrapping the list object we need to initialize it as part of the ListStack class.
+        """
+        super().__init__()
+        self._list = []
+
+    def push(self, item):
+        """
+        Docstring for moduleFour.ListStack.push()
+
+        Description:
+            - Adds a new item to the stack.
+            - Uses the append() list method since we are wrapping the list object.
+        """
+        self._list.append(item)
+
+    def pop(self):
+        """
+        Docstring for moduleFour.ListStack.pop()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Removes and RETURNS the item at the top of the stack.
+            - raises an error it the user attempts to pop from an empty stack.
+        """
+        try:
+            return self._list.pop()
+        except IndexError:
+            raise IndexError("Can not pop from empty list")
+
+    def peek(self):
+        return self._list[-1]
+
+    def __len__(self):
+        return len(self._list)
+
+    def is_empty(self):
+        return len(self._list) == 0
+
+class Queue(ABC):
+    """
+    Docstring for moduleFour.Queue
+
+    Description:
+        - A list with the restriction that insertion and deletion can only be preformed from one end.
+        - Insertion/Enqueue can only be preformed from the rear/end/top/head/back of the list of the list.
+        - Deletion/Dequeue can only be preformed from the front/beginning of the list.
+        - FIFO
+
+    Main Operations:
+        * Enqueue(Item)         # TIME COMPLEXITY: O(1)
+        * Dequeue()             # TIME COMPLEXITY: O(n)
+        * Peek() or Front()     # TIME COMPLEXITY: O(1)
+        * IsEmpty()             # TIME COMPLEXITY: O(1)
+        * Len()                 # TIME COMPLEXITY: O(1)
+
+    Queue Diagram:
+    Queue = [1,2,3,4,5,6,7]
+             ^           ^
+             Front       Back
 
     Enqueue(new element) --- > (Queue) ---> Dequeue(old element)
 
-    Problem: dequeue runs in O(n), which is not good.
+    Problem: Dequeue() runs in O(n), which is not good.
     """
+
+    @abstractmethod
     def __init__(self):
-        self._L = []
- 
+        """
+        Docstring for moduleFour.Queue(Queue).__init__()
+
+        When used as a wrapper (for a list as an example) it would init self.L = []
+        since it is imposing rules on the list object [] is would need to wrap around the list []
+        Get it?
+        """
+        return
+
+    @abstractmethod
     def enqueue(self, item):
-        self._L.append(item)
- 
+        """
+        Docstring for moduleFour.Queue.enqueue()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Adds a new item to the back of the queue [to the back of the list].
+
+        Example:
+        Queue = [1,2,3,4,5,6,7]
+                             ^
+                             Back of Queue.
+        Queue.enqueue(8)
+        Queue = [1,2,3,4,5,6,7,8]
+                                ^
+                                Added 8 to the back of the queue
+        """
+        return
+
+    @abstractmethod
     def dequeue(self):
-        return self._L.pop(0)
- 
+        """
+        Docstring for moduleFour.Queue.dequeue()
+
+        TIME COMPLEXITY: O(n)
+        Description:
+            - Removes the item from the front of the queue. [from index 0 of the list, so the front of the list]
+            - It runs of O(n) because once the first item is removed, all the items in the queue have to shift up one.
+
+        Example:
+        Queue = [1,2,3,4,5,6,7]
+        Queue.dequeue()
+        Queue = [2,3,4,5,6,7]
+        RETURNS: 1
+        """
+        return
+
+    @abstractmethod
     def peek(self):
-        return self._L[0]
- 
+        """
+        Docstring for moduleFour.Queue.peek()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Returns the item at the top of the queue without affecting the Queue.
+
+        Example:
+        Queue = [1,2,3,4,5,6,7]
+        Queue.peek()
+        RETURNS: 1
+        """
+        return
+
+    @abstractmethod
     def __len__(self):
-        return len(self._L)
- 
-    def isempty(self):
-        return len(self) == 0
+        """
+        Docstring for moduleFour.Queue.__len__()
 
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Returns the number of items in the queue.
+        """
+        return
 
-class LazyQueue(Queue):
+    @abstractmethod
+    def is_empty(self):
+        """
+        Docstring for moduleFour.Queue.is_empty()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Returns True if the queue is empty.
+        """
+        return
+
+class LazyListQueue(Queue):
     """
-    Docstring for ListQueueFakeDelete
+    Docstring for moduleFour.LazyQueue(Queue)
 
-    Defintion: 
-    A subclass of Queue that implement lazy dequeue
-    modifies the dequeue function in order to run in O(1) not O(n)
+    Description:
+        - This is an implementation of Queue as list wrapper and a more efficient/lazy .dequeue() method.
+        - Modifies .dequeue() method to run in O(1) instead of O(n)
+        - Dequeue now instead of actually removing element from list, it changes the head by one
 
-
-    Instead of actullay removing element from list, it changes the head by one
-    
-    
-    [1,2,3,4,5,6]
-     ^
-     Head
-
-    Dequeue()
-
+    Lazy Dequeue Method Diagram:
+    Queue = [1,2,3,4,5,6]
+             ^
+             Front
+    Queue.dequeue()
     [1,2,3,4,5,6]
        ^
        New Head
     """
 
     def __init__(self):
+        """
+        Docstring for moduleFour.LazyLazyQueue(Queue)
+        """
         super().__init__()
-        self._head = 0 #Head variable is created to move the head instead of remove the element
+        self._head = 0 #Head variable is created to move the head instead of remove the elements.
+        self._list = []
         
-    
+    def enqueue(self, item):
+        """
+        Docstring for moduleFour.Queue.enqueue()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Adds a new item to the back of the queue [to the back of the list].
+        """
+        self._list.append(item)
+
     def dequeue(self):
-        item = self.peek()
+        """
+        Docstring for moduleFour.Queue.dequeue()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Moves the head in queue in order to the item after the "removed" item creating a new head.
+            - When list gets half empty, we will set the head back to 0.
+
+        Dequeue Method Diagram:
+        Queue = [1,2,3,4,5,6]
+                 ^
+                 Head
+        Queue.dequeue()
+        Queue = [1,2,3,4,5,6]
+                   ^ New Head
+        RETURNS: 1
+
+        Dequeue Method Clean Up Diagram:
+        Queue = [1,2,3,4,5,6,7,8]
+                       ^
+                       Head
+        Queue.dequeue()
+        Since the head is now greater than len(Queue)//2, the list is reinitialized to include only the current head element and everything after.
+        Queue = [5,6,7,8]
+                 ^
+                 New Head
+        RETURNS: 4
+        """
+        item = self._list[self._head]
         self._head += 1
+        if self._head > len(self._list)//2:
+            self._list = self._list[self._head:]
+            self._head = 0
         return item
 
     def peek(self):
-        return self._L[self._head]
+        return self._list[self._head]
 
     def __len__(self):
-        return len(self._L) - self._head
-    
-    """
-    Takes less time for delete since it doesnt have to go to the begining of the list
-    """
+        return len(self._list) - self._head
 
-class Deque(Stack and Queue):
-    """
-    Docstring for Deque
+    def is_empty(self):
+        return len(self._list) - self._head == 0
 
-    Defintion: An abstract data type that acts like a double-ended queue/ acts like both a Stack and a Queue at the same time.
+class Deque(ABC):
+    """
+    Docstring for moduleFour.Deque
+
+    Description:
+        - Basically a stack and a queue combined.
+        - Can add an remove items from both sides of the deck
+        - An abstract data type that acts like a double-ended queue/ acts like both a Stack and a Queue at the same time.
 
     Acts both like a Stack and a Queue
 
     Main Operations:
-        * addfirst(item)    O(n) adds an item to the front of the deque
-        * addlast(item)     O(1)
-        * removefirst(item) O(n)
-        * removelast(item)  O(1)
-    
-        
-
+        * AddFirst(item)    O(n) adds an item to the front of the deque.
+        * AddLast(item)     O(1) adds an item to the back of the deque.
+        * RemoveFirst(item) O(n) removes item from the front of the deque.
+        * RemoveLast(item)  O(1) removes item from the back of the deque.
+        * Len()             O(1) returns the length of the deque.
     """
 
-    pass
+    def __init__(self):
+        """
+        Docstring for moduleFour.Deque.__init__()
+        """
+        pass
 
-class LinkedList:
+    @abstractmethod
+    def add_first(self, item):
+        """
+        Docstring for moduleFour.Deque.add_first()
+
+        TIME COMPLEXITY: O(n)
+        Description:
+            - Adds a new item to the front of the deque.
+
+        Example:
+            Deque = [1,2,3,4,5,6,7]
+            Deque.add_first(0)
+            Deque = [0,1,2,3,4,5,6,7]
+        """
+        return
+
+    @abstractmethod
+    def add_last(self, item):
+        """
+        Docstring for moduleFour.Deque.add_last()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Adds a new item to the back of the deque.
+
+        Example:
+            Deque = [1,2,3,4,5,6,7]
+            Deque.add_last(8)
+            Deque = [1,2,3,4,5,6,7,8]
+        """
+        return
+
+    @abstractmethod
+    def remove_first(self):
+        """
+        Docstring for moduleFour.Deque.remove_first()
+
+        TIME COMPLEXITY: O(n)
+        Description:
+            - Removes the first item from the deque.
+
+        Example:
+            Deque = [1,2,3,4,5,6,7]
+            Deque.remove_first()
+            Deque = [2,3,4,5,6,7]
+            RETURNS: 1
+        """
+        return
+
+    @abstractmethod
+    def remove_last(self):
+        """
+        Docstring for moduleFour.Deque.remove_last()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Removes the last item from the deque.
+
+        Example:
+        Deque = [1,2,3,4,5,6,7]
+        Deque.remove_last()
+        Deque = [1,2,3,4,5,6]
+        RETURNS: 7
+        """
+
+    def __len__(self):
+        """
+        Docstring for moduleFour.Deque.__len__()
+
+        TIME COMPLEXITY: O(1)
+        Description:
+            - Returns the length of the deque.
+        """
+        return
+
+class ListDeque(Deque):
     """
-    Docstring for LinkedList
+    Docstring for moduleFour.ListDeque
 
-    Definition: a structure we will use to create a more efficient implimentaion of the ADT deque
+    Description:
+        - Deque implementation as a list wrapper.
+    """
 
-    Components of a LinkedList:
-        * Node:
+    def __init__(self):
+        super().__init__()
+        self._list =[]
 
+    def add_first(self, item):
+        self._list.append(item)
+
+    def add_last(self, item):
+        self._list.insert(0,item)
+
+    def remove_first(self):
+        return self._list.pop(0)
+
+    def remove_last(self):
+        return self._list.pop()
+
+    def __len__(self):
+        return len(self._list)
+
+#==== Data Structures ====#
+
+class SinglyLinkedList:
+    """
+    Docstring for moduleFour.SinglyLinkedList
+
+    Description:
+        - A datastructures used to create more efficient implementation of ADT Deque.
+        - Storing data using a series of Node.
+        - One Node points to the next node in the Series.
+    Components of a SinglyLinkedList:
+        * LinkedList class
+        * Node class
+
+    SinglyLinkedList Diagram:
+    SinglyLinkedList = node1(data,node2) --> node2(data,node3) ---> node3(data,None)
+    !!!Note: This is not a list wrapper, these nodes are not stored within a list object!!!
         
     Problems:
+        * Nodes can only access previous Nodes
 
-    Nodes can only access prevoius Nodes
-
-
-    Review:
-
-    Each element is a seperate object and they will be linked like a chain
-
-    [node1(data,none),node2(data,points to node1)]
-
-    given [node1,node2,node3,node4,node5]
-            ^
-            Head
-
-    add_first(node0)
-
-    now [node0,node2,node3,node4,node5]
-            ^   
-            New head
-
-    node2 now needs to point towards 
-
-
-
-
-
+    Each element is a separate object, and they will be linked like a chain
     """
 
-    class Node:
+    @dataclass
+    class SinglyLinkedListNode:
         """
-        Docstring for Node
+        Docstring for moduleFour.SinglyLinkedList.SinglyLinkedListNode
 
-        Each Node points to the previous Node in the linked list
+        Description:
+            -
+        The Node class does not have any methods, they are ment to just store and point
         """
-
         def __init__(self,data,link=None):
+            """
+            Docstring for moduleFour.SinglyLinkedList.SinglyLinkedListNode.__init__()
+            """
             self.data = data
             self.link = link
             return
 
-    class LinkedList:
-        
-        def __init__(self):
-            self._head = None
+    def __init__(self):
+        """
+        Docstring for moduleFour.SinglyLinkedList.__init__()
 
-        def addfirst(self,item):
-            self._head = LinkedList.Node(item,self._head)
-            return
+        Description:
+            -
+        """
+        self._head = None
+        self._tail = None
+        self._size = 0
+
+    def add_first(self, item):
+        """
+        Docstring for moduleFour.SinglyLinkedList.add_first()
+        """
+        self._head = SinglyLinkedList.SinglyLinkedListNode(item,self._head)
+        if self._tail is None: self._tail = self._head
+        self._size += 1
+
+    def add_last(self, item):
+        self._tail.link = SinglyLinkedList.SinglyLinkedListNode(item)
+
+
+    def remove_first(self, item):
+        """
+        Docstring for moduleFour.SinglyLinkedList.remove_first()
+        """
+        item = self._head.data
+        self._head = self._head.link
+        return item
+
+
+
 
     
 #---- 02-20-2026 Lecuture Notes ----#
--- Notes/test.py
+++ Notes/test.py
@@ -1,3 +1,28 @@
-student_id = "STU00001"
+class Stack():
+    def __init__(self):
+        self._list = []
 
-print(student_id[0:3])
+    def push(self, item):
+        self._list.append(item)
+
+    def pop(self):
+        try:
+            return self._list.pop()
+        except:
+            raise ValueError("Can not pop from an empty stack")
+
+    def __len__(self):
+        return len(self._list)
+
+    def is_empty(self):
+        return len(self._list) == 0
+
+s = Stack()
+
+s.push(1)
+s.push(2)
+s.push(3)
+
+print(s.pop(),s.pop(),s.pop())
+
+print(s.is_empty())
