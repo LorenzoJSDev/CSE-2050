5ff60ba3c9ec97132fefd92817561d244d8f522c
Lorenzo Serrano <lorenzo.serrano@ibm.com>
2026-04-07 12:09:08 -0400

    test

--- Notes/moduleSeven/moduleSeven.py
+++ Notes/moduleSeven/moduleSeven.py
@@ -1,4 +1,17 @@
 """
-BLANK
+<moduleSeven.py>
 
+-------------------------------------------------------------
+Notes for CSE 2050 module Seven which covers:
+
+Divide and Conquer:
+
+
+-------------------------------------------------------------
+
+Author: Lorenzo .S
+Contributors: [" "]
+Created: 04-07-2026
+Last Updated: 04-07-2026
 """
+
