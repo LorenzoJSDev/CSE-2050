758b8637f94140ebb8ecdb6129727fd24a626a7a
Lorenzo Serrano <lorenzo.serrano@ibm.com>
2026-04-08 14:51:53 -0400

    HELPPPP MEEEEE, HELLLPPPPP!

---
+++ Notes/moduleNine.py
@@ -0,0 +1,25 @@
+"""
+
+Data Structures:
+    * Trees
+
+Vocab
+
+Node types of a Tree:
+    * Root: No parents
+    * Internals Nodes: Nodes with a Parent and a child
+    * Leaf: Nodes with no children
+
+Length of a path: The number of edges between two nodes
+Edges: the arrows pointing between nodes
+
+Tree Types:
+    *Full Binary Tree: Each Node has at either two or 0 children
+    * Complete Binary Tree: Binary tree with each level full, following left to right node pattern
+
+Traversal Methods of a Tree from starting point:
+    * Pre-Order traverse: Root -> Left -> Right
+    * In-Order traverse: Left -> Root -> Right
+    * Post-Order traverse: Left -> Right -> Root
+
+"""
